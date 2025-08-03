4187489b134bee3fe6538a8c062d234952fde7a2
nwxxb <nawwabzia@gmail.com>
2025-08-03 14:54:14 +0700

    Squashed commit of the following: (and remove todo)
    commit 23b84e6b997250365b57d2bc3e20fa4227791a24
    Author: nwxxb <nawwabzia@gmail.com>
    Date:   Sun Aug 3 14:52:10 2025 +0700
    
        use the newly created dict in stdout
    
    commit c82dc7be3588b078691f6a66bd598845687cca5f
    Author: nwxxb <nawwabzia@gmail.com>
    Date:   Sun Aug 3 14:51:23 2025 +0700
    
        init basic locale and helloworld dict

--- test.rb
+++ test.rb
@@ -1,3 +1,10 @@
-puts 'halo dunia'.capitalize
+CURRENT_LOCALE ||= 'id'
+
+hello_world_dict = {
+  'id' => 'halo dunia',
+  'end' => 'hello world'
+}
+
+puts hello_world_dict[CURRENT_LOCALE].capitalize
 
 puts 'yet another stdout line 1'.capitalize
