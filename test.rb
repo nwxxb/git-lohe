45bbc3e12561f991549f2ed4b547bc1b1ce18e51
nwxxb <nawwabzia@gmail.com>
2025-08-03 15:33:19 +0700

    open ARGV and fix typo

--- test.rb
+++ test.rb
@@ -1,10 +1,11 @@
-current_locale ||= 'id'
+current_locale = ARGV.join(' ').match(/--locale=(\w\w)/).to_a[1]
+current_locale = current_locale.nil? || current_locale == '' ? 'en' : current_locale
 
 hello_world_dict = {
   'id' => 'halo dunia',
-  'end' => 'hello world'
+  'en' => 'hello world'
 }
 
-puts hello_world_dict[current_locale].capitalize
+puts (hello_world_dict[current_locale] || 'hello world fallback').capitalize
 
 puts 'yet another stdout line 1'.capitalize
