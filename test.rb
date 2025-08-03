648903c97b2d4ef60d39f24206cd46bfdbfd013c
nwxxb <nawwabzia@gmail.com>
2025-08-03 15:20:07 +0700

    remove todo comments and change current locale constant to a variable

--- test.rb
+++ test.rb
@@ -1,11 +1,10 @@
-# TODO: how to dynamically set this locale from cli?
-CURRENT_LOCALE ||= 'id'
+current_locale ||= 'id'
 
 hello_world_dict = {
   'id' => 'halo dunia',
   'end' => 'hello world'
 }
 
-puts hello_world_dict[CURRENT_LOCALE].capitalize
+puts hello_world_dict[current_locale].capitalize
 
 puts 'yet another stdout line 1'.capitalize
